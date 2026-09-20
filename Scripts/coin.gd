aa7b6586c30edc2b724479f4ef9d35968935d446
XxR4V3NxX <133625303+XxR4V3NxX@users.noreply.github.com>
2026-09-19 18:48:58 -0600

    Todo menos Assets
    Importacion del proyecto de Plataformer 2D en Godot para la tarea
    Note: Falta la carpeta assets

---
+++ Scripts/coin.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		body.add_coin()
+		queue_free()
---
+++ Scripts/ui.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+
+@onready var coin_label = $Control/Label
+
+func set_coins(amount):
+	coin_label.text = str(amount)
